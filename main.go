335f497a8561ffac2232143b6b5194743a4af1d3
husnimbark <husnibarking@gmail.com>
2022-08-29 12:18:28 +0700

    update

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"waysbucks/database"
+	"waysbucks/pkg/mysql"
 	"waysbucks/routes"
 
 	"github.com/gorilla/handlers"
@@ -12,24 +14,32 @@ import (
 )
 
 func main() {
-
+	// env
 	errEnv := godotenv.Load()
 	if errEnv != nil {
 		panic("Failed to load env file")
 	}
 
+	// initial DB
+	mysql.DatabaseInit()
+
+	// run migration
 	database.RunMigration()
 
 	r := mux.NewRouter()
 
 	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
 
+	//path file
+	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+
 	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
 	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
 
-	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+	// Modify 1 line this below code get port from env ...
+	var port = os.Getenv("PORT")
 
-	fmt.Println("Server Running on localhost:4000")
-	http.ListenAndServe("localhost:4000", handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+	fmt.Println("server running localhost:" + port)
+	http.ListenAndServe(":"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
 }
